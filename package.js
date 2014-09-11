494414e8c6425fa3df2f890103a06dbc4fecdd9d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-09-11 14:28:29 +0530

    added functionality

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  summary: "use this.unblock inside publications",
+  version: "1.0.0",
+  git: "https://github.com/meteorhacks/unblock.git",
+  name: "meteorhacks:unblock"
+});
+
+Package.onUse(function (api, where) {
+  configurePackages(api);
+});
+
+Package.onTest(function(api) {
+  configurePackages(api);
+  api.versionsFrom('METEOR@0.9.0');
+  api.use('tinytest');
+  api.addFiles('test/unblock.js', 'server');
+});
+
+function configurePackages (api) {
+  api.use('meteorhacks:meteorx@1.0.2');
+  api.addFiles('lib/unblock.js', 'server');
+}
---
+++ test/unblock.js
@@ -0,0 +1,61 @@
+Tinytest.addAsync('this.unblock availability', function(test, done) {
+  var id = Random.id();
+  var conn = DDP.connect(process.env.ROOT_URL);
+  Meteor.publish(id, function() {
+    test.equal(typeof this.unblock, 'function');
+    done();
+    conn.close();
+  });
+
+  conn.subscribe(id);
+});
+
+Tinytest.addAsync('functionality check - wait forever without unblock', function(test, done) {
+  var id1 = Random.id();
+  var id2 = Random.id();
+  var conn = DDP.connect(process.env.ROOT_URL);
+  var subProcessing = [];
+
+  Meteor.publish(id1, function() {
+    subProcessing.push(id1);
+    Meteor._sleepForMs(1000 * 60 * 60);
+  });
+
+  Meteor.publish(id2, function() {
+    subProcessing.push(id2);
+  });
+
+  conn.subscribe(id1);
+  conn.subscribe(id2);
+
+  Meteor.setTimeout(function() {
+    test.equal(subProcessing, [id1]);
+    done();
+    conn.close();
+  }, 400);
+});
+
+Tinytest.addAsync('functionality check - no waiting with unblock', function(test, done) {
+  var id1 = Random.id();
+  var id2 = Random.id();
+  var conn = DDP.connect(process.env.ROOT_URL);
+  var subProcessing = [];
+
+  Meteor.publish(id1, function() {
+    subProcessing.push(id1);
+    this.unblock();
+    Meteor._sleepForMs(1000 * 60 * 60);
+  });
+
+  Meteor.publish(id2, function() {
+    subProcessing.push(id2);
+
+    test.equal(subProcessing, [id1, id2]);
+    done();
+    conn.close();
+  });
+
+  conn.subscribe(id1);
+  conn.subscribe(id2);
+
+});
