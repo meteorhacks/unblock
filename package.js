2b0734e775d54e5cc2284cc772bfb5c7243473c0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-09-19 10:57:39 +0530

    added support for 0.8.3

--- package.js
+++ package.js
@@ -1,22 +1,28 @@
 Package.describe({
-  summary: "use this.unblock inside publications",
-  version: "1.0.2",
+  summary: "a way to use this.unblock inside publications",
+  version: "1.1.0",
   git: "https://github.com/meteorhacks/unblock.git",
   name: "meteorhacks:unblock"
 });
 
-Package.onUse(function (api, where) {
+Package.on_use(function (api, where) {
   configurePackages(api);
 });
 
-Package.onTest(function(api) {
+Package.on_test(function(api) {
   configurePackages(api);
-  api.versionsFrom('METEOR@0.9.0');
+
   api.use('tinytest');
-  api.addFiles('test/unblock.js', 'server');
+  api.add_files('test/unblock.js', 'server');
 });
 
 function configurePackages (api) {
-  api.use('meteorhacks:meteorx@1.0.2');
-  api.addFiles('lib/unblock.js', 'server');
+  // only on Meteor 0.9+
+  if(api.versionsFrom) {
+    api.versionsFrom('METEOR@0.9.0');
+    api.use('meteorhacks:meteorx@1.0.2');
+  } else {
+    api.use('meteorx');
+  }
+  api.add_files('lib/unblock.js', 'server');
 }
